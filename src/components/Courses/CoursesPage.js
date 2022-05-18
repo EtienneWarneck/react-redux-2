93e423998bc65b5f9e7bcd98b27f900d418b858b
Etienne Warneck <etienne.warneck@gmail.com>
2022-05-17 22:03:23 -0400

    adding thunk to load data

--- src/components/Courses/CoursesPage.js
+++ src/components/Courses/CoursesPage.js
@@ -1,43 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from "react-redux";
-import TextInput from '../common/TextInput';
 import * as courseActions from '../../redux/actions/courseActions';
 import { bindActionCreators } from 'redux';
 
 function CoursesPage(props) {
-    const [course, setCourse] = useState({
-        title: ""
-    })
 
-    function handleChange({ target }) {
-        setCourse({
-            ...course,
-            [target.name]: target.value
-        })
-    };
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        props.actions.createCourse(course)
-    }
+    useEffect(() => {
+        props.actions.loadCourses()
+    });
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <h2>Courses</h2>
-                <h3>Add course</h3>
-                <TextInput
-                    id="title"
-                    label="Title"
-                    onChange={handleChange}
-                    name="title"
-                />
-                <input type="submit" value="save" />
-                {props.courses.map(course => (
-                    <div key={course.title}>{course.title}</div>
-                ))}
-            </form>
-        </div>
+        <>
+            <h2>Courses</h2>
+            {props.courses.map(course => (
+                <div key={course.title}>{course.title}</div>
+            ))}
+        </>
     );
 }
 
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from "./reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk"
 
 export default function configureAppStore(initialState) {
     const store = configureStore({
         reducer: rootReducer,
-        middleware: [reduxImmutableStateInvariant()],
+        middleware: [reduxImmutableStateInvariant(), thunk],
         enhancers: []
     })
     return store
